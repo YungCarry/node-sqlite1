eee62c5926cadd245fe3696a23caa25d6ee5d2fd
YungCarry <keridodorobi@gmail.com>
2024-11-29 10:12:22 +0100

    asdasd

--- swagger.js
+++ swagger.js
@@ -1,13 +1,13 @@
 import swaggerAutogen from 'swagger-autogen';
 const doc = {
   info: {
-    title: 'Users API',
+    title: 'Swagger API',
     version: "1.0.0",
-    description: 'Users API'
+    description: 'First swagger file'
   },
   host: 'localhost:3000',
   basePath: "/api/users"
 };
-const outputFile = './swagger-output.json';
+const outputFile = './swaggerDocs.json';
 const routes = ['./routes/users.js'];
 swaggerAutogen()(outputFile, routes, doc);
